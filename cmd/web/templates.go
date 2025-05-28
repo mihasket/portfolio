4d8f780ab70ec81c8f0a451281a6d9f983c3984b
mihasket <miha.sket@gmail.com>
2025-05-28 19:42:39 +0200

    feat: caching components

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -18,14 +18,17 @@ func newTemplateCache() (map[string]*template.Template, error) {
 		fmt.Printf("%v \n", page)
 		name := filepath.Base(page)
 
-		files := []string{
-			"./ui/html/base.tmpl",
-			"./ui/html/components/waybar.tmpl",
-			"./ui/html/components/zsh.tmpl",
-			page,
+		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		if err != nil {
+			return nil, err
 		}
 
-		ts, err := template.ParseFiles(files...)
+		ts, err = ts.ParseGlob("./ui/html/components/*.tmpl")
+		if err != nil {
+			return nil, err
+		}
+
+		ts, err = ts.ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
