ff6b0163b13dc452fe4a8f5dcbe81ab7c32af4f7
mihasket <miha.sket@gmail.com>
2025-06-05 21:49:55 +0200

    feat: http.server struct error logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -31,8 +31,9 @@ func main() {
 	}
 
 	srv := &http.Server{
-		Addr:    *addr,
-		Handler: app.routes(),
+		Addr:     *addr,
+		Handler:  app.routes(),
+		ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
 	}
 
 	logger.Info("starting server", "addr", srv.Addr)
