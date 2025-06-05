bf3e5b0550ef548aabc1fae2fb5e865c523a2cbf
mihasket <miha.sket@gmail.com>
2025-06-05 21:35:39 +0200

    feat: panic recovery (single goroutine)

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,5 +11,5 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("GET /{$}", app.home)
 	mux.HandleFunc("GET /projects/{$}", app.projects)
 
-	return app.logRequests(commonHeaders(mux))
+	return app.recoverPanic(app.logRequests(commonHeaders(mux)))
 }
